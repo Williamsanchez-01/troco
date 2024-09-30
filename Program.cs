1acff7a3cf29668ffac694fbbafa35c0d8e935bf
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-09-30 12:10:47 -0300

    finaliza exercicio troco

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Qual o valor do produto?\n");
+double vl = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine($"\n o valor do seu produto lhe custou {vl}\n");
+
+
+Console.WriteLine("\n Agora digite por favor o valor pago por favor.\n");
+double vltr = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine($"\n você usou R${vltr} para pagar o produto.\n");
+
+
+double sub = vltr - vl;
+
+
+Console.WriteLine($"\n Seu troco {sub}");
